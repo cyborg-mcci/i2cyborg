815640e7a02bf875057559391365f5aa05da0db2
IOwnACastle <piotrcanine777@gmail.com>
2023-07-11 12:16:46 +0100

    Read and Wait Sequences
    Added the read and wait sequences to I2CRegs.py. (Not yet tested)

--- DDisc_PythonMaster/I2CRegs.py
+++ DDisc_PythonMaster/I2CRegs.py
@@ -26,20 +26,22 @@ while (inc<length):     # While loop to execute the functions of the regVals fil
     
     line = read[inc-1]
     val1 = line[0]
+
+    inc +=1
     
     if val1 == '1':
 
-        regR = read[inc]
+        regR = read[inc-1]
         regNum = int(regR[0])
         
-        inc = inc+2
+        inc +=1
 
         reginc = 0
         while (reginc < regNum):
-            inc = inc+1
             reginc = reginc+1
 
-            reg1 = read[inc-2]
+            reg1 = read[inc-1]
+            inc +=1
             hexreg = re.findall(r'[0-9A-Fa-f]+', reg1)   #Finding the registers and data in the text file
 
             sREG = hexreg[0]         # Coverting string to HEX, and assigning the Regs and data to variables REG and Data
@@ -54,10 +56,42 @@ while (inc<length):     # While loop to execute the functions of the regVals fil
            
             # i2c.i2cWrite(dwf, hdwf, nak, address, reg, data)    #
             nak = i2c.i2cWrite(dwf, hdwf, nak, Addr, iREG, iData)
-        
-        time.sleep(1e-6)
-        inc = inc+2
+
     
+    elif val1 == '2':   # READ SEQUENCE
+
+        regR = read[inc-1]
+        regNum = int(regR[0])
+            
+        inc +=1
+
+        reginc = 0
+
+        while (reginc < regNum):
+                
+            reginc = reginc+1
+
+            reg1 = read[inc-1]
+            inc +=1
+
+            hexreg = re.findall(r'[0-9A-Fa-f]+', reg1)
+            sREG = hexreg[0]
+            iREG = int(sREG, 16)
+            REG = hex(iREG)
+            print("Register:", REG)   
+
+            # i2c.i2cRead(dwf, hdwf, nak, slave_address, reg)
+            dataR, nak = i2c.i2cRead(dwf, hdwf, nak, Addr, iREG)
+        
+    elif val1 == '3':   # WAIT SEQUENCE
+        
+        wait_line = read[inc-1]
+        wait_timeS = re.findall(r'\d+', wait_line)
+        wait_time = int(wait_timeS[0])
+
+        
+        time.sleep(wait_time*1e-6)
+        inc = inc+1
     
     else:
         print("Error in text")  
--- DDisc_PythonMaster/incr1.py
+++ DDisc_PythonMaster/incr1.py
@@ -13,20 +13,23 @@ while (inc<length):
     
     line = read[inc-1]
     val1 = line[0]
-    
-    if val1 == '1':
 
-        regR = read[inc]
+    inc +=1
+    
+    if val1 == '1':   # WRITE SEQUENCE
+
+        regR = read[inc-1]
         regNum = int(regR[0])
         
-        inc = inc+2
+        inc +=1
 
         reginc = 0
         while (reginc < regNum):
-            inc = inc+1
+            
             reginc = reginc+1
 
-            reg1 = read[inc-2]
+            reg1 = read[inc-1]
+            inc +=1
 
             hexreg = re.findall(r'[0-9A-Fa-f]+', reg1)
             sREG = hexreg[0]
@@ -38,9 +41,46 @@ while (inc<length):
             # Print the extracted values
             print("Register:", REG)
             print("Data:    ", Data)
+
         
-        time.sleep(1e-6)
-        inc = inc+2
+            
+        
+        #time.sleep(1e-6)
+        #inc = inc+2
+    
+    elif val1 == '2':   # READ SEQUENCE
+
+        regR = read[inc-1]
+        regNum = int(regR[0])
+        
+        inc +=1
+
+        reginc = 0
+
+        while (reginc < regNum):
+            
+            reginc = reginc+1
+
+            reg1 = read[inc-1]
+            inc +=1
+
+            hexreg = re.findall(r'[0-9A-Fa-f]+', reg1)
+            sREG = hexreg[0]
+            iREG = int(sREG, 16)
+            REG = hex(iREG)
+            print("Register:", REG)
+        
+
+    
+    elif val1 == '3':   # WAIT SEQUENCE
+        
+        wait_line = read[inc-1]
+        wait_timeS = re.findall(r'\d+', wait_line)
+        wait_time = int(wait_timeS[0])
+
+        
+        time.sleep(wait_time*1e-6)
+        inc = inc+1
     
     
     else:
