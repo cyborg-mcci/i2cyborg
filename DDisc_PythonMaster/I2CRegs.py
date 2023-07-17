813038b641e1b7b8b963e7552c0ac17b93cb8a4b
IOwnACastle <piotrcanine777@gmail.com>
2023-07-17 16:59:39 +0100

    I2CRegs.py changes to use confirmWrite function

--- DDisc_PythonMaster/I2CRegs.py
+++ DDisc_PythonMaster/I2CRegs.py
@@ -5,21 +5,22 @@ from dwfconstants import *
 import time
 import i2cFunctions as i2c
 
+#CONFIGURATION
 Addr = 0x45            # SLAVE ADDRESS 
+RateSet = 100e3        # Clock Rate
+i2cSCL = 24            # SCL Pin Set
+i2cSDA = 25            # SDA Pin Set
 
 dwf = i2c.loadDwf()     #Loads DWF library for i2c communication
-
 hdwf = i2c.openDevice(dwf)   #Opens DDD and creates a device handle for the device called hdwf
-
-# i2c.i2cConfig(dwf, hdwf, RateSet, SCL SDA)
-nak = i2c.i2cConfig(dwf, hdwf, 10e3, 24, 25)
+nak = i2c.i2cConfig(dwf, hdwf, RateSet, i2cSCL, i2cSDA)
 
 file = open('shortReg.txt', 'r')   #Command to open the txt file
-
 read = file.readlines()
 global inc
 inc = 1
 length = len(read)
+
 while (inc<length):     # While loop to execute the functions of the regVals file
 
     print('-----------------------------------------------------------------------')
@@ -31,18 +32,18 @@ while (inc<length):     # While loop to execute the functions of the regVals fil
     
     if val1 == '1':     # WRITE SEQUENCE
 
-        regRead = read[inc-1]
-        regNum = int(regR[0])
+        reg_line_Read = read[inc-1]
+        reg_number = int(regR[0])
         
         inc +=1
 
         reginc = 0
-        while (reginc < regNum):
+        while (reginc < reg_number):
             reginc = reginc+1
 
-            reg1 = read[inc-1]
+            data_line = read[inc-1]
             inc +=1
-            hexreg = re.findall(r'[0-9A-Fa-f]+', reg1)   #Finding the registers and data in the text file
+            hexreg = re.findall(r'[0-9A-Fa-f]+', data_line)   #Finding the registers and data in the text file
 
             sREG = hexreg[0]         # Coverting string to HEX, and assigning the Regs and data to variables REG and Data
             sData = hexreg[1]
@@ -50,38 +51,37 @@ while (inc<length):     # While loop to execute the functions of the regVals fil
             iData = int(sData, 16)
             REG = hex(iREG)
             Data = hex(iData)
-           
-            print("Register:", REG)    # Print the extracted values
-            print("Data:    ", Data)
+
+            print("Register: {:04X}\tData:{:04X}".format(REG, Data))
            
             # i2c.i2cWrite(dwf, hdwf, nak, address, reg, data)    #
-            nak = i2c.i2cWrite(dwf, hdwf, nak, Addr, iREG, iData)
+            nak = i2c.i2cWriteConfirm(dwf, hdwf, nak, Addr, iREG, iData)
 
     
     elif val1 == '2':   # READ SEQUENCE
 
-        regRead = read[inc-1]
-        regNum = int(regR[0])
+        reg_line_read = read[inc-1]
+        reg_number = int(regR[0])
             
         inc +=1
 
         reginc = 0
 
-        while (reginc < regNum):
+        while (reginc < reg_number):
                 
             reginc = reginc+1
 
-            reg1 = read[inc-1]
+            data_line = read[inc-1]
             inc +=1
 
-            hexreg = re.findall(r'[0-9A-Fa-f]+', reg1)
+            hexreg = re.findall(r'[0-9A-Fa-f]+', data_line)
             sREG = hexreg[0]
             iREG = int(sREG, 16)
             REG = hex(iREG)
-            #print("Register:", REG)   
-
+            
             # i2c.i2cRead(dwf, hdwf, nak, slave_address, reg)
             dataR, nak = i2c.i2cRead(dwf, hdwf, nak, Addr, iREG)
+            print("Register: {:04X}\tData:{:04X}".format(REG, dataR))
         
     elif val1 == '3':   # WAIT SEQUENCE
         
